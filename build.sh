6853312d954c99a2b259bd48fc972d6a58096a77
Ильнур Нигматуллин <54642871+paomb@users.noreply.github.com>
2023-10-12 18:08:20 +0300

    - delete latest tag - add amd arm tags

--- build.sh
+++ build.sh
@@ -1,20 +1,19 @@
-[[ -z "$1" ]] && { echo "Укажи тег для нового образа!\nСуществующие теги:" && curl -L -s 'https://registry.hub.docker.com/v2/repositories/paomb/yc-iac/tags?page_size=1024' | jq '."results"[]["name"]' ; exit 1; }
-[[ -z "$2" ]] && { echo "Опубликовать образ в hub.docker.com: y or n" ; exit 1; }
+[[ -z "$1" ]] && { echo "Specify the tag for the new image!\nExisting tags:" && curl -L -s 'https://registry.hub.docker.com/v2/repositories/paomb/yc-iac/tags?page_size=1024' | jq '."results"[]["name"]' | sort ; exit 1; }
+[[ -z "$2" ]] && { echo "Publish an image to hub.docker.com: y or n" ; exit 1; }
 tag=$1
 option=$2
-docker buildx build --platform linux/amd64 -t paomb/yc-iac:$tag  .
-docker buildx build --platform linux/arm64 -t paomb/yc-iac:$tag-arm  .
+docker buildx build --platform linux/amd64 -t paomb/yc-iac:$tag-amd64  .
+docker buildx build --platform linux/arm64 -t paomb/yc-iac:$tag-arm64  .
 
 case $option in 
 y)
-docker push paomb/yc-iac:$tag
-docker tag paomb/yc-iac:$tag paomb/yc-iac:latest
-docker tag paomb/yc-iac:$tag-arm paomb/yc-iac:arm
-docker push paomb/yc-iac:latest
-docker push paomb/yc-iac:arm
-echo "docker run -ti --rm  paomb/yc-iac:latest bash"
+docker push paomb/yc-iac:$tag-amd64
+docker push paomb/yc-iac:$tag-arm64
+echo "docker run -ti --rm  paomb/yc-iac:$tag-amd64 bash"
+echo "docker run -ti --rm  paomb/yc-iac:$tag-arm64 bash"
 ;;
+
 n) echo "Образ не опубликован в hub.docker.com"
-echo "docker run -ti --rm  paomb/yc-iac:$tag bash"
-;;
+echo "docker run -ti --rm  paomb/yc-iac:$tag-amd64 bash"
+echo "docker run -ti --rm  paomb/yc-iac:$tag-arm64 bash";;
 esac
